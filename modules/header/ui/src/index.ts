8aaf0fa185eb854fb714653a0d1d2884caf46d14
WhiteK <khanh96.ng@gmail.com>
2024-11-29 00:28:55 +0800

    Add some todos

--- modules/header/ui/src/index.ts
+++ modules/header/ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/account-avatar/account-avatar.component';
 export * from './lib/account-menu/account-menu.component';
+export * from './lib/account-quick-info/account-quick-info.component';
 export * from './lib/center-header/center-header.component';
 export * from './lib/end-header/end-header.component';
 export * from './lib/login-button/login-button.component';
