d60abd30fa200a8506717f42818eebd7369c81b9
WhiteK <khanh96.ng@gmail.com>
2025-05-02 02:05:45 +0700

    refactor: replace effect with afterRenderEffect for improved icon directive rendering

--- modules/shared/ui/src/lib/directives/icon/icon.directive.ts
+++ modules/shared/ui/src/lib/directives/icon/icon.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import { afterRenderEffect, Directive, inject, input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 //TODO convert all icon to use mat-icon
@@ -19,16 +19,18 @@ export class IconDirective {
   transform = input<string | undefined>(undefined);
 
   constructor() {
-    effect(() => {
-      if (this.viewBox() != null) {
-        this.host._elementRef.nativeElement
-          .getElementsByTagName('svg')[0]
-          .setAttribute('viewBox', this.viewBox() as string);
-      }
-      if (this.transform() != null) {
-        this.host._elementRef.nativeElement.style.transform =
-          this.transform() as string;
-      }
+    afterRenderEffect({
+      write: () => {
+        if (this.viewBox() != null) {
+          this.host._elementRef.nativeElement
+            .getElementsByTagName('svg')[0]
+            .setAttribute('viewBox', this.viewBox() as string);
+        }
+        if (this.transform() != null) {
+          this.host._elementRef.nativeElement.style.transform =
+            this.transform() as string;
+        }
+      },
     });
   }
 }
