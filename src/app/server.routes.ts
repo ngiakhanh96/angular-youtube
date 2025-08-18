49231c47f74aa34b2259cc16e41cfd652ba64c2b
WhiteK <khanh96.ng@gmail.com>
2025-08-18 23:03:12 +0800

    fix: change renderMode from Prerender to Server for the root path in serverRoutes

--- src/app/server.routes.ts
+++ src/app/server.routes.ts
@@ -3,7 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
   {
     path: 'watch',
