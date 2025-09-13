82a8aab053ac45f9a2f9c51badeb87ca953f3bcd
WhiteK <khanh96.ng@gmail.com>
2025-09-14 01:16:10 +0800

    fix(eslint): update ESLint configuration to improve TypeScript and Nx integration

--- eslint.base.config.mjs
+++ eslint.base.config.mjs
@@ -1,28 +1,167 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
 import nxEslintPlugin from '@nx/eslint-plugin';
-import ngrxEslintPlugin from '@ngrx/eslint-plugin';
+import ngrxEslintPlugin from '@ngrx/eslint-plugin/v9';
 import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
 import prettierConfig from 'eslint-config-prettier';
 import tsEslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
-const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
-});
-
 export default defineConfig([
+  ...nxEslintPlugin.configs['flat/base'],
+  ...nxEslintPlugin.configs['flat/typescript'],
+  ...nxEslintPlugin.configs['flat/javascript'],
   {
     ignores: ['**/dist'],
   },
   {
-    plugins: {
-      '@nx': nxEslintPlugin,
-      '@ngrx': ngrxEslintPlugin,
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+          depConstraints: [
+            {
+              sourceTag: 'scope:details-page-feature',
+              onlyDependOnLibsWithTags: [
+                'scope:details-page-ui',
+                'scope:details-page-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:header-feature',
+              onlyDependOnLibsWithTags: [
+                'scope:header-ui',
+                'scope:header-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:home-page-feature',
+              onlyDependOnLibsWithTags: [
+                'scope:home-page-ui',
+                'scope:home-page-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:search-page-feature',
+              onlyDependOnLibsWithTags: [
+                'scope:search-page-ui',
+                'scope:search-page-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:shell-feature',
+              onlyDependOnLibsWithTags: [
+                'scope:*-feature',
+                'scope:*-data-access',
+                'scope:shell-ui',
+                'scope:shared-ui',
+              ],
+            },
+            {
+              sourceTag: 'scope:sidebar-feature',
+              onlyDependOnLibsWithTags: [
+                'scope:sidebar-ui',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:details-page-ui',
+              onlyDependOnLibsWithTags: [
+                'scope:details-page-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:header-ui',
+              onlyDependOnLibsWithTags: [
+                'scope:header-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:home-page-ui',
+              onlyDependOnLibsWithTags: [
+                'scope:home-page-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:search-page-ui',
+              onlyDependOnLibsWithTags: [
+                'scope:search-page-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:shell-ui',
+              onlyDependOnLibsWithTags: [
+                'scope:shell-data-access',
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:sidebar-ui',
+              onlyDependOnLibsWithTags: [
+                'scope:shared-ui',
+                'scope:shared-data-access',
+              ],
+            },
+            {
+              sourceTag: 'scope:shared-ui',
+              onlyDependOnLibsWithTags: ['scope:shared-data-access'],
+            },
+            {
+              sourceTag: 'scope:details-page-data-access',
+              onlyDependOnLibsWithTags: ['scope:shared-data-access'],
+            },
+            {
+              sourceTag: 'scope:header-data-access',
+              onlyDependOnLibsWithTags: ['scope:shared-data-access'],
+            },
+            {
+              sourceTag: 'scope:home-page-data-access',
+              onlyDependOnLibsWithTags: ['scope:shared-data-access'],
+            },
+            {
+              sourceTag: 'scope:search-page-data-access',
+              onlyDependOnLibsWithTags: ['scope:shared-data-access'],
+            },
+            {
+              sourceTag: 'scope:shell-data-access',
+              onlyDependOnLibsWithTags: ['scope:shared-data-access'],
+            },
+            {
+              sourceTag: 'scope:shared-data-access',
+              onlyDependOnLibsWithTags: [],
+            },
+            {
+              sourceTag: 'scope:master',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
     },
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [...ngrxEslintPlugin.configs.signals],
+    rules: {},
   },
   ...tsEslint.configs.recommendedTypeChecked.map((config) => ({
     ...config,
@@ -30,211 +169,23 @@ export default defineConfig([
   })),
   {
     files: ['**/*.ts'],
-
     // This is required, see the docs
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname, // or import.meta.dirname for ESM
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
-    files: ['src/**/*.ts', 'modules/**/*.ts'],
+    files: ['**/*.ts'],
     rules: {
       ...tsEslintPlugin.configs.recommended.rules,
     },
   },
-  ...compat
-    .config({
-      extends: ['plugin:@ngrx/signals'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-      rules: {
-        ...config.rules,
-        '@nx/enforce-module-boundaries': [
-          'error',
-          {
-            enforceBuildableLibDependency: true,
-            allow: [],
-            depConstraints: [
-              {
-                sourceTag: 'scope:details-page-feature',
-                onlyDependOnLibsWithTags: [
-                  'scope:details-page-ui',
-                  'scope:details-page-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:header-feature',
-                onlyDependOnLibsWithTags: [
-                  'scope:header-ui',
-                  'scope:header-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:home-page-feature',
-                onlyDependOnLibsWithTags: [
-                  'scope:home-page-ui',
-                  'scope:home-page-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:search-page-feature',
-                onlyDependOnLibsWithTags: [
-                  'scope:search-page-ui',
-                  'scope:search-page-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:shell-feature',
-                onlyDependOnLibsWithTags: [
-                  'scope:*-feature',
-                  'scope:*-data-access',
-                  'scope:shell-ui',
-                  'scope:shared-ui',
-                ],
-              },
-              {
-                sourceTag: 'scope:sidebar-feature',
-                onlyDependOnLibsWithTags: [
-                  'scope:sidebar-ui',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:details-page-ui',
-                onlyDependOnLibsWithTags: [
-                  'scope:details-page-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:header-ui',
-                onlyDependOnLibsWithTags: [
-                  'scope:header-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:home-page-ui',
-                onlyDependOnLibsWithTags: [
-                  'scope:home-page-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:search-page-ui',
-                onlyDependOnLibsWithTags: [
-                  'scope:search-page-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:shell-ui',
-                onlyDependOnLibsWithTags: [
-                  'scope:shell-data-access',
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:sidebar-ui',
-                onlyDependOnLibsWithTags: [
-                  'scope:shared-ui',
-                  'scope:shared-data-access',
-                ],
-              },
-              {
-                sourceTag: 'scope:shared-ui',
-                onlyDependOnLibsWithTags: ['scope:shared-data-access'],
-              },
-              {
-                sourceTag: 'scope:details-page-data-access',
-                onlyDependOnLibsWithTags: ['scope:shared-data-access'],
-              },
-              {
-                sourceTag: 'scope:header-data-access',
-                onlyDependOnLibsWithTags: ['scope:shared-data-access'],
-              },
-              {
-                sourceTag: 'scope:home-page-data-access',
-                onlyDependOnLibsWithTags: ['scope:shared-data-access'],
-              },
-              {
-                sourceTag: 'scope:search-page-data-access',
-                onlyDependOnLibsWithTags: ['scope:shared-data-access'],
-              },
-              {
-                sourceTag: 'scope:shell-data-access',
-                onlyDependOnLibsWithTags: ['scope:shared-data-access'],
-              },
-              {
-                sourceTag: 'scope:shared-data-access',
-                onlyDependOnLibsWithTags: [],
-              },
-              {
-                sourceTag: 'scope:master',
-                onlyDependOnLibsWithTags: ['*'],
-              },
-            ],
-          },
-        ],
-      },
-    })),
-  ...compat
-    .config({
-      extends: ['plugin:@nx/typescript'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
-      rules: {
-        ...config.rules,
-      },
-    })),
-  ...compat
-    .config({
-      extends: ['plugin:@nx/javascript'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
-      rules: {
-        ...config.rules,
-      },
-    })),
-  ...compat
-    .config({
-      env: {
-        jest: true,
-      },
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
-      rules: {
-        ...config.rules,
-      },
-    })),
-  prettierConfig,
   {
     files: ['**/*.js'],
     extends: [tsEslint.configs.disableTypeChecked],
   },
+  prettierConfig,
 ]);
